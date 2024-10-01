8d6b7c19e8f50ef252169ab1d84352c4ae66226a
Jesper Sørensen <jeppester1@gmail.com>
2024-10-01 23:03:54 +0200

    Make duplicity s3 settings more generic

--- home/core/restore.sh
+++ home/core/restore.sh
@@ -1,6 +1,6 @@
 #!/usr/bin bash
 echo "Checking for backup";
-duplicity collection-status --s3-endpoint-url ${SCW_ENDPOINT_URL} --s3-region-name ${SCW_REGION} ${SCW_BUCKET} | grep "Found primary backup"
+duplicity collection-status --s3-endpoint-url ${AWS_ENDPOINT_URL} --s3-region-name ${AWS_REGION} ${AWS_BUCKET} | grep "Found primary backup"
 FOUND_BACKUP=$?;
 
 if [ $FOUND_BACKUP -eq 0 ]; then
@@ -8,7 +8,7 @@ if [ $FOUND_BACKUP -eq 0 ]; then
   echo "Emptying state to prepare for restore";
   rm -rf /state/*
   echo "Restoring backup"
-  duplicity restore --s3-endpoint-url ${SCW_ENDPOINT_URL} --s3-region-name ${SCW_REGION} ${SCW_BUCKET} /state;
+  duplicity restore --s3-endpoint-url ${AWS_ENDPOINT_URL} --s3-region-name ${AWS_REGION} ${AWS_BUCKET} /state;
   exit $?;
 else
   echo "Backup not present, continuing";
